46b4800c86b9ee56123118a78ca72ce6ac51aecc
juamorrio <jmorenorios5@gmail.com>
2025-05-02 11:48:20 +0200

    little fix in BookingRecord repository

--- src/main/java/acme/features/customer/bookingRecord/CustomerBookingRecordRepository.java
+++ src/main/java/acme/features/customer/bookingRecord/CustomerBookingRecordRepository.java
@@ -27,10 +27,10 @@ public interface CustomerBookingRecordRepository extends AbstractRepository {
 	@Query("SELECT br FROM BookingRecord br WHERE br.passenger.id = :passengerId")
 	Collection<BookingRecord> findBookingRecordByPassengerId(@Param("passengerId") Integer passengerId);
 
-	@Query("select p from Passenger p where p.customer.id = :customerId and p.draftMode = false")
+	@Query("select p from Passenger p where p.customer.userAccount.id = :customerId and p.draftMode = false")
 	Collection<Passenger> findAllPublishedPassengersByCustomerId(@Param("customerId") int customerId);
 
-	@Query("select b from Booking b where b.customer.id = :customerId and b.draftMode = true")
+	@Query("select b from Booking b where b.customer.userAccount.id = :customerId and b.draftMode = true")
 	Collection<Booking> findAllDraftBookingsByCustomerId(@Param("customerId") int customerId);
 
 }
